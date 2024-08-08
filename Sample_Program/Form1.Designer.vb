f93f907e2e6100ee458b6077366f5f2d8bcc3fbd
unknown <Administrator@cb.cadopt.com>
2024-08-08 16:38:06 +0530

    Sameple Code

---
+++ Sample_Program/Form1.Designer.vb
@@ -0,0 +1,197 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Text4 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Text2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Text3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Text1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Text6 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Text5 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Text4
+        '
+        Me.Text4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Text4.Location = New System.Drawing.Point(96, 104)
+        Me.Text4.Name = "Text4"
+        Me.Text4.Size = New System.Drawing.Size(75, 20)
+        Me.Text4.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 106)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(54, 15)
+        Me.Label4.TabIndex = 337
+        Me.Label4.Text = "Capacity"
+        '
+        'Text2
+        '
+        Me.Text2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Text2.Location = New System.Drawing.Point(96, 52)
+        Me.Text2.Name = "Text2"
+        Me.Text2.Size = New System.Drawing.Size(75, 20)
+        Me.Text2.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 54)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 15)
+        Me.Label2.TabIndex = 335
+        Me.Label2.Text = "Pressure"
+        '
+        'Text3
+        '
+        Me.Text3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Text3.Location = New System.Drawing.Point(96, 78)
+        Me.Text3.Name = "Text3"
+        Me.Text3.Size = New System.Drawing.Size(75, 20)
+        Me.Text3.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 80)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(41, 15)
+        Me.Label3.TabIndex = 333
+        Me.Label3.Text = "Temp."
+        '
+        'Text1
+        '
+        Me.Text1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Text1.Location = New System.Drawing.Point(96, 26)
+        Me.Text1.Name = "Text1"
+        Me.Text1.Size = New System.Drawing.Size(75, 20)
+        Me.Text1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 15)
+        Me.Label1.TabIndex = 331
+        Me.Label1.Text = "Project ID"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(96, 182)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 29)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "Proceed"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Text6
+        '
+        Me.Text6.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Text6.Location = New System.Drawing.Point(96, 156)
+        Me.Text6.Name = "Text6"
+        Me.Text6.Size = New System.Drawing.Size(75, 20)
+        Me.Text6.TabIndex = 6
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(12, 158)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(61, 15)
+        Me.Label6.TabIndex = 342
+        Me.Label6.Text = "Tube Thk."
+        '
+        'Text5
+        '
+        Me.Text5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Text5.Location = New System.Drawing.Point(96, 130)
+        Me.Text5.Name = "Text5"
+        Me.Text5.Size = New System.Drawing.Size(75, 20)
+        Me.Text5.TabIndex = 5
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(12, 132)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(56, 15)
+        Me.Label5.TabIndex = 340
+        Me.Label5.Text = "Tube OD"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(211, 224)
+        Me.Controls.Add(Me.Text6)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Text5)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Text4)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Text2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Text3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Text1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Component Drum"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Text4 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Text2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Text3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Text1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Text6 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Text5 As TextBox
+    Friend WithEvents Label5 As Label
+End Class
---
+++ Sample_Program/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim Project As String
+        Dim Pressure As Double
+        Dim Temp As Double
+        Dim Capacity As Double
+        Dim TubeOD As Double
+        Dim TubeThk As Double
+        Dim TubeID As Double
+
+        Project = Text1.Text
+        Pressure = Text2.Text
+        Temp = Text3.Text
+        Capacity = Text4.Text
+        TubeOD = Text5.Text
+        TubeThk = Text6.Text
+
+        TubeID = TubeOD - (2 * TubeThk)
+
+        MsgBox("Tube ID - " & TubeID, vbOKOnly)
+
+        End
+
+    End Sub
+End Class
